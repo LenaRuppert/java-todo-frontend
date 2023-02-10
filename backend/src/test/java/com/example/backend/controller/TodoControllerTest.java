8709d6881f133fd3b893c8001f5382494ea45e61
lenaruppert <l.zi@gmx.de>
2023-02-10 18:08:22 +0100

    friday evening

---
+++ backend/src/test/java/com/example/backend/controller/TodoControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoControllerTest {
+
+    @Test
+    void getToDoList() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    void addToDo() {
+    }
+}
---
+++ backend/src/test/java/com/example/backend/service/ToDoServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoServiceTest {
+
+    @Test
+    void getToDoList() {
+    }
+
+    @Test
+    void addToDo() {
+    }
+}
