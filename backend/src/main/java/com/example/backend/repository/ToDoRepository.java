a49ea5949b7e3400c511e42c7506b99c76429a55
lenaruppert <l.zi@gmx.de>
2023-02-10 18:08:10 +0100

    Changes by edonakrosaislami

--- backend/src/main/java/com/example/backend/repository/ToDoRepository.java
+++ backend/src/main/java/com/example/backend/repository/ToDoRepository.java
@@ -5,11 +5,45 @@ import com.example.backend.model.ToDo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Repository
 @AllArgsConstructor
 public class ToDoRepository {
 
     private final List<ToDo> toDoList;
+
+
+    public ToDo getToDo(String id){
+        for(ToDo todo: toDoList){
+            if(todo.id() == id){
+                return todo;
+            }
+        }
+        throw new NoSuchElementException("id not found");
+
+    }
+
+    public List<ToDo> getToDoList() {return toDoList;
+    }
+
+    public ToDo addToDo(ToDo toDo){
+        toDoList.add(toDo);
+        return toDo;
+    }
+
+
+    public ToDo changeToInProgress(String id) {
+        Status status =getToDo(id).status().IN_PROGRESS;
+        ToDo toChange= new ToDo(getToDo(id).description(), status, id);
+        return toChange;
+    }
+
+    public ToDo deleteToDo(String id) {
+        toDoList.remove(getToDo(id));
+        return getToDo(id);
+    }
+
 }
--- backend/src/main/java/com/example/backend/service/ToDoService.java
+++ backend/src/main/java/com/example/backend/service/ToDoService.java
@@ -1,12 +1,34 @@
 package com.example.backend.service;
 
+import com.example.backend.model.ToDo;
 import com.example.backend.repository.ToDoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ToDoService {
 
     private final ToDoRepository toDoRepository;
+    private final IdService idService;
+
+
+public List<ToDo> getToDoList() {return toDoRepository.getToDoList();
+    }
+
+    public ToDo addToDo(ToDo toDo){
+        ToDo toDoToAdd=new ToDo(toDo.description(), toDo.status(),idService.generateId());
+        toDoRepository.addToDo(toDoToAdd);
+        return  toDoToAdd;
+    }
+    public ToDo changeToInProgress(String id){
+        return toDoRepository.changeToInProgress(id);
+
+    }
+
+    public ToDo deleteToDo(String id){
+        return toDoRepository.deleteToDo(id);
+    }
 }
